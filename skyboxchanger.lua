c5d8eeaa735239d67b57886dab7dbf43c09f3082
makbee777-cloud <makbee777@gmail.com>
2026-07-07 11:53:04 +0500

    Add skyboxchanger.lua to change the skybox

---
+++ skyboxchanger.lua
@@ -0,0 +1,10 @@
+local Lighting = game:GetService("Lighting")
+
+local sky = Instance.new("Sky")
+sky.SkyboxBk = "rbxassetid://YOUR_ASSET_ID"
+sky.SkyboxDn = "rbxassetid://YOUR_ASSET_ID"
+sky.SkyboxFt = "rbxassetid://YOUR_ASSET_ID"
+sky.SkyboxLf = "rbxassetid://YOUR_ASSET_ID"
+sky.SkyboxRt = "rbxassetid://YOUR_ASSET_ID"
+sky.SkyboxUp = "rbxassetid://YOUR_ASSET_ID"
+sky.Parent = Lighting
